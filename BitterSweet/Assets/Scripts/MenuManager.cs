96ba5bd8131793223cac9d2d64128071deff2c96
Rune Finlayson <43809212+Runeatic@users.noreply.github.com>
2021-10-11 13:39:14 +1100

    swdf

--- BitterSweet/Assets/Scripts/MenuManager.cs
+++ BitterSweet/Assets/Scripts/MenuManager.cs
@@ -6,35 +6,44 @@ using UnityEngine.SceneManagement;
 public class MenuManager : MonoBehaviour
 {
 	GameObject[] menuObjects;
-	//GameObject[] pauseObjects;
+	GameObject[] pauseObjects;
 	GameObject[] creditObjects;
+	GameObject[] dialogueObjects;
 
 	void Start()
 	{
 		Time.timeScale = 0;
 		menuObjects = GameObject.FindGameObjectsWithTag("Main Menu");
 		creditObjects = GameObject.FindGameObjectsWithTag("Credits");
-		//pauseObjects = GameObject.FindGameObjectsWithTag("Pause Menu");
+		pauseObjects = GameObject.FindGameObjectsWithTag("Pause Menu");
+		dialogueObjects = GameObject.FindGameObjectsWithTag("Dialogue");
 		hideCredits();
+		hidePauseMenu();
+		hideDialogue();
 	}
 
-	//void Update()
-	//{
-	//	if (Input.GetKeyDown(KeyCode.Escape))
-	//	{
-	//		if (Time.timeScale == 1)
-	//		{
-	//			Time.timeScale = 0;
-	//			showMenu();
-	//		}
-	//		else if (Time.timeScale == 0)
-	//		{
-	//			Debug.Log("game is go");
-	//			Time.timeScale = 1;
-	//			hideMenu();
-	//		}
-	//	}
-	//}
+	void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.Escape))
+		{
+			if (Time.timeScale == 1)
+			{
+				Time.timeScale = 0;
+				showPauseMenu();
+			}
+			else if (Time.timeScale == 0)
+			{
+				Debug.Log("game is go");
+				Time.timeScale = 1;
+				hidePauseMenu();
+			}
+		}
+	}
+
+	public void LoadLevel()
+	{
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+	}
 
 	public void Quit()
 	{
@@ -67,8 +76,35 @@ public class MenuManager : MonoBehaviour
 		}
 	}
 
-	public void LoadLevel()
+	public void showPauseMenu()
 	{
-		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+		foreach (GameObject g in pauseObjects)
+		{
+			g.SetActive(true);
+		}
+	}
+
+	public void hidePauseMenu()
+	{
+		foreach (GameObject g in pauseObjects)
+		{
+			g.SetActive(false);
+		}
+	}
+
+	public void showDialogue()
+    {
+		foreach (GameObject g in dialogueObjects)
+        {
+			g.SetActive(true);
+        }
+    }
+
+	public void hideDialogue()
+	{
+		foreach (GameObject g in dialogueObjects)
+        {
+			g.SetActive(false);
+        }
 	}
 }
