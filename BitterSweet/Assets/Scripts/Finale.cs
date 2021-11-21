9a69ccfa40e778017392e7550dceb168793b8964
Rune Finlayson <43809212+Runeatic@users.noreply.github.com>
2021-11-22 00:03:44 +1100

    final final final fainl

--- BitterSweet/Assets/Scripts/Finale.cs
+++ BitterSweet/Assets/Scripts/Finale.cs
@@ -1,16 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using System;
 
 public class Finale : MonoBehaviour
 {
-    private MenuManager menuManager;
+    GameObject[] finaleObjects;
+    	string playersName;
+	public TextMeshProUGUI finaleTxt;
 
+    void Start()    
+    {
+        finaleObjects = GameObject.FindGameObjectsWithTag("Finale");
+    }
         private void OnTriggerEnter(Collider col)
     {
-        if (col.gameObject.tag == "Player")
-        {
-            menuManager.showFinale();
-        }
+		finaleTxt.text = "There is nothing left for you to do " + playersName + ".";
+		foreach (GameObject g in finaleObjects)
+		{
+			g.SetActive(true);
+		}
     }
 }
